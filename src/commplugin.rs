d1007e8b555a775696d352a8e482b296ac6a27be
David Venhoek <david@tweedegolf.com>
2021-03-12 08:10:22 +0100

    Modified authresult to represent content of jwt.

--- src/commplugin.rs
+++ src/commplugin.rs
@@ -5,8 +5,9 @@ use serde::{Serialize, Deserialize};
 pub struct StartCommRequest {
     /// Purpose of the communication session
     pub purpose: String,
-    /// Attribute JWE containing attributes provided by an earlier authentication request
-    pub attributes: Option<String>,
+    /// JWT containing authentication session results provided by an earlier authentication request
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub auth_result: Option<String>,
 }
 
 /// Expected result for a start_communication request
@@ -15,5 +16,6 @@ pub struct StartCommResponse {
     /// URL for client to start communication process
     pub client_url: String,
     /// URL to which authentication result will/should be sent after completion of authentication flow
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub attr_url: Option<String>,
 }
