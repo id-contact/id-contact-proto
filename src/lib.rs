15a5645605dee3a1e2c6ad16a89455114ecae77a
Henk Dieter Oordt <hd@oordt.dev>
2021-08-11 16:24:22 +0200

    export AuthSelectParams

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,5 @@ mod common;
 
 pub use authplugin::{StartAuthRequest, StartAuthResponse};
 pub use authresult::{AuthResult, AuthStatus, SessionActivity};
-pub use commplugin::{StartCommRequest, StartCommResponse};
+pub use commplugin::{StartCommRequest, StartCommResponse, AuthSelectParams};
 pub use common::{SessionOptions, StartRequestAuthOnly, ClientUrlResponse};
